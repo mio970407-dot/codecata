1d86184354f90a535a71be90e1e46e8d24bee777
Y2Y <mio970407@gmail.com>
2026-01-02 09:40:30 +0900

    code 260102

---
+++ code 260102a.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    if (n > 0 && n <= 1000)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 2 == 0)
+                answer += i;
+        }
+    }
+    return answer;
+}
