a59f2f77eabf076c3a2140a706b944d5c7738a66
Y2Y <mio970407@gmail.com>
2026-01-28 09:44:04 +0900

    code 260128

---
+++ code 260128b.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 0;
+    for (int i = 0; i <= 9; i++) {
+        auto a = find(numbers.begin(), numbers.end(), i);
+        if (a != numbers.end()) {
+            answer += 0;
+        }
+        else {
+            answer += i;
+        }
+    }
+    return answer;
+}
