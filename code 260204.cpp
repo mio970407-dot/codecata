01e0e510256d9e8e61adc9bfc3f3a5d5299bf789
Y2Y <mio970407@gmail.com>
2026-02-04 09:56:13 +0900

    Create code 260204.cpp

---
+++ code 260204.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = (right + left) * (right - left + 1) / 2;
+    for (int i = 1; i <= 100; i++) {
+        for (int j = left; j <= right; j++) {
+            if (i * i == j) {
+                answer -= j * 2;
+            }
+        }
+    }
+    return answer;
+}
