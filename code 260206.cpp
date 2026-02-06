f274d526d1200e5713bb947486d796faa3d6c544
Y2Y <mio970407@gmail.com>
2026-02-06 09:13:57 +0900

    Create code 260206.cpp

---
+++ code 260206.cpp
@@ -0,0 +1,10 @@
+﻿using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = (long long)price * (1 + count) * count / 2 - money;
+    if (answer <= 0) {
+        answer = 0;
+    }
+    return answer;
+}
