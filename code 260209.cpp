e578c626d19137a58a25e880d776217277f54a65
Y2Y <mio970407@gmail.com>
2026-02-09 10:23:13 +0900

    Create code 260209.cpp

---
+++ code 260209.cpp
@@ -0,0 +1,18 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    if (s.size() != 4 && s.size() != 6) {
+        answer = false;
+    }
+    for (int i = 0; i < s.length(); i++) {
+        if (48 > s[i] || s[i] > 57) {
+            answer = false;
+        }
+    }
+
+    return answer;
+}
