1ec5d0b30ea9b38383a943e94f11df4068a4c8f4
Y2Y <mio970407@gmail.com>
2026-02-11 09:10:23 +0900

    Create code 260211.cpp

---
+++ 0211to0410/code 260211.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    for (int m = 0; m < b; m++) {
+        for (int n = 0; n < a; n++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
