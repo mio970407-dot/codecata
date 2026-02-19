680714e24148fae5adfa9f616b73aa9527ab3eba
Y2Y <mio970407@gmail.com>
2026-02-19 09:31:51 +0900

    Create code 260219.cpp

---
+++ 0211to0410/code 260219.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    int idx = 0;
+    string answer = s;
+    for (int i = 0; i < answer.length(); i++) {
+        if (answer[i] == ' ') {
+            idx = 0;
+            continue;
+        }
+        if (idx % 2 == 0) {
+            answer[i] = toupper(answer[i]);
+        }
+        else {
+            answer[i] = tolower(answer[i]);
+        }
+        idx++;
+    }
+    return answer;
+}
