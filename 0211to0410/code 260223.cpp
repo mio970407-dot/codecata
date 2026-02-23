acb62b033d163873768c24acd1e11ce4cb3436d4
Y2Y <mio970407@gmail.com>
2026-02-23 10:03:08 +0900

    Create code 260223.cpp

---
+++ 0211to0410/code 260223.cpp
@@ -0,0 +1,15 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string t, string p) {
+    int answer = 0;
+    for (int i = 0; i <= t.length() - p.length(); i++) {
+        string newt = t.substr(i, p.length());
+        if (newt <= p) {
+            answer++;
+        }
+    }
+    return answer;
+}
